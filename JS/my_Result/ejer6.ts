dab36499961d29d0d854d8a2f087fcc8c48248af
Ezequiel Sosa <108107012+000EZE000@users.noreply.github.com>
2023-01-13 19:21:05 -0300

    realizado ejercicio 6

---
+++ JS/my_Result/ejer6.ts
@@ -0,0 +1,10 @@
+const paintSquare=(num=1)=>{
+const vertice='* ** *';
+const lado =  '*    *';
+console.log(vertice)
+while (num - 1 !== 0) {
+  console.log(lado);
+  num--;
+}
+console.log(vertice);
+}
